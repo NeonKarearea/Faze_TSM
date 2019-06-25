f0fd528667cd0fc20b7605fb276769ec6d25c332
Neon Karearea <46880415+NeonKarearea@users.noreply.github.com>
2019-06-26 08:40:39 +1200

    Update FazeTSM.js

--- FazeTSM.js
+++ FazeTSM.js
@@ -1,17 +1,1 @@
-var modal = document.getElementById("Modal");
-var btn = document.getElementById("btn");
-var span = document.getElementById("close")[0];
 
-btn.onclick = function() {
-	modal.style.display = "block";
-}
-
-span.onclick = function() {
-	modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-	if(event.target == modal) {
-		modal.style.display = "none";
-	}
-}
